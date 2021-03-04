a5c32f036718406264f37b143cdc93dbebcd8c16
Eyeanman <Ian.Hartwell1986@gmail.com>
2021-03-04 11:07:45 +0000

    Created long lived token generator

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # coinmetro-api-tools
 Tools which can be used with the Coinmetro API
+
+## get_token.py
+Create a long lived token to be used on future API calls.
+
+### How to use
+* Execute get_token.py using Python3
+* When Prompted, complete the following:
+    * Email Address: Your Coinmetro Email Address used to login
+    * Password: Your Coinmetro Password
+    * Device Name: Friendly Name you are using this token for (e.g. Home Assistant)
+    * OTP: Your 6 digit On-Time-Passcode from your authenticator app
+* Take note of your Token and DeviceId as these are both required for all API calls
+* Check your Email or goto your Coinmetro Settings to authorise app (make sure to compare the PIN number provided as they should match)
+
+### TODO
+* API available to auto autherise device however was not working, appears to be newly listed so may not yet be active
---
+++ get_token.py
@@ -0,0 +1,79 @@
+import requests
+import uuid
+import random
+
+random_id = ' '.join([str(random.randint(0, 999)).zfill(3) for _ in range(2)])
+
+apiRoot = "https://api.coinmetro.com"
+
+def main():
+    pin = generate_pin()
+    deviceid = generate_deviceid()
+    login = get_userinput("Email Address")
+    password = get_userinput("Password")
+    devicename = get_userinput("Device Name")
+    OTP = get_userinput("OTP")
+    token = get_token(login, password, OTP, deviceid, devicename, pin)
+    print(f"The PIN number was: {pin}")
+    print(f"Your Token is: {token}")
+    print(f"Your DeviceId is: {deviceid}")
+
+
+def generate_deviceid():
+    response = uuid.uuid4().hex
+    return response
+
+
+def get_userinput(data):
+    response = input(f"Enter {data}:")
+    return response
+
+
+def generate_pin():
+    random_id = str(random.randint(0, 999999))
+    return random_id
+
+
+def request_api(method, api, headers, payload):
+    response = requests.request(method, f"{apiRoot}{api}", headers = headers, data = payload).json()
+    return response
+
+
+def get_token(login, password, OTP, deviceid, devicename, pin):
+    payload = f'login={login}&password={password}'
+    headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'X-OTP': OTP,
+    'X-Device-Id': deviceid,
+    }
+    response = request_api("POST", "/jwtDevice", headers, payload)
+    if response["message"] == "New Device":
+        print("Token Generated, updating device info")
+        token = response["token"]
+        update_deviceinfo(token, deviceid, devicename, pin)
+        return token
+    else:
+        print(f"Error: {response['message']}")
+        raise SystemExit(0)
+
+
+def update_deviceinfo(token, deviceid, devicename, pin):
+    payload = f'deviceName={devicename}&accessToken={token}&PIN={pin}'
+    headers = {
+        'X-Device-ID': deviceid,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    response = request_api("POST", "/devices", headers, payload)
+    if response["message"] == "PIN updated":
+        return response
+    else:
+        print(f"Error: {response['message']}")
+        raise SystemExit(0)
+    return (response)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
